f2f81bf8137494cb06b1d653f6b87b3db28df44c
Rishabh <22rishu.rk01@gmail.com>
2026-07-22 00:27:27 +0530

    change in cors

--- task-manager-backend/index.js
+++ task-manager-backend/index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://6a5fbdf4fc84d6609634a9a1--dazzling-mousse-f53346.netlify.app"
     ]
 }))
 app.use(express.json());
