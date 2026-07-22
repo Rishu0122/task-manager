71d5008a29af77d3efd17a82691487241165c33a
Rishabh <22rishu.rk01@gmail.com>
2026-07-22 13:01:20 +0530

    update link in frontend

--- task-manager-backend/index.js
+++ task-manager-backend/index.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://6a5fbdf4fc84d6609634a9a1--dazzling-mousse-f53346.netlify.app"
+        "https://6a5fc9f50ccbeb3b2282b850--dazzling-mousse-f53346.netlify.app"
     ]
 }))
 app.use(express.json());
